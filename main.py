089ce5b6051339660a279ef8bb84ba034e9b8da0
snhkn <senihaakin@hotmail.com>
2023-07-05 18:59:40 +0300

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Seniha")
+    content = """
+        Greetings! I'm Seniha, and I have an immense passion for cats. 
+        Presently, I am nurturing four adorable baby cats. 
+        Alongside that, I'm actively seeking an exciting job opportunity.
+        I hold a degree in Computer Science, although I've taken a break from the workforce for a while. 
+        As a way to showcase my capabilities, I've created this portfolio website. 
+        Thank you for your interest! Feel free to explore and learn more about my skills and experiences.
+        """
+    st.info(content)
