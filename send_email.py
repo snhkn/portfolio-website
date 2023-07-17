89e5404cffe548ca0d419d08fd2f137d81f6ba3a
snhkn <senihaakin@hotmail.com>
2023-07-17 12:41:01 +0300

    Send emails using the form

--- send_email.py
+++ send_email.py
@@ -1,20 +1,15 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-user_name = "snhknprojects@gmail.com"
-password = "qkbxdjxutnxinkea"
+    user_name = "snhknprojects@gmail.com"
+    password = "qkbxdjxutnxinkea"
 
-receiver = "snhknprojects@gmail.com"
-context = ssl.create_default_context()
+    receiver = "snhknprojects@gmail.com"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(user_name, password)
-    server.sendmail(user_name, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
